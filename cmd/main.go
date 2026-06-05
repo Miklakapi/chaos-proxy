96c8ecb40f831d547f59809d8f71f0de7d27bc54
kacper.miklaszewski <kkmiklaszewski@gmail.com>
2026-06-05 12:35:06 +0200

    init

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	appCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	config := struct {
+		Port string
+	}{
+		Port: "5050",
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		_, err := fmt.Fprint(w, "ok")
+		if err != nil {
+			log.Printf("failed to write response %v", err)
+		}
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + config.Port,
+		Handler: mux,
+	}
+
+	go func() {
+		log.Println("HTTP server started on :" + config.Port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen error: %v", err)
+		}
+	}()
+
+	<-appCtx.Done()
+	log.Println("shutdown signal received")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Printf("graceful shutdown failed: %v, forcing close", err)
+
+		if err := srv.Close(); err != nil {
+			log.Printf("forced server close failed: %v", err)
+		}
+	}
+
+	log.Println("server stopped gracefully")
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Miklakapi/chaos-proxy
+
+go 1.26.3
+
+require gopkg.in/yaml.v3 v3.0.1 // indirect
